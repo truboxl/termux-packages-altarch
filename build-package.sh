ff329be5090b5d53c7fd97456c30163f99d3d5ee
Jia Yuan Lo <jylo06g@gmail.com>
2022-08-28 21:46:56 +0800

    ci: split patches and update workflow

--- build-package.sh
+++ build-package.sh
@@ -10,8 +10,9 @@ git reset --hard
 case "$1" in
 arm-vfpv3-d16)
 	TERMUX_ARCH=arm
-	patch -p1 -i "$REPOROOT/termux-packages.$1.patch"
-	./scripts/run-docker.sh ./scripts/setup-android-sdk.sh
+	patch -p1 -i "$REPOROOT/termux-packages.$1.lf.patch"
+	patch -p1 -i "$REPOROOT/termux-packages.$1.crlf.patch"
+	./scripts/run-docker.sh env TERMUX_NDK_VERSION_NUM=23 TERMUX_NDK_REVISION=c ./scripts/setup-android-sdk.sh
 	;;
 *)
 	echo "[!] ERROR: Unsupported option $1"
--- generate-bootstraps.sh
+++ generate-bootstraps.sh
@@ -10,7 +10,8 @@ git reset --hard
 case "$1" in
 arm-vfpv3-d16)
 	TERMUX_ARCH=arm
-	patch -p1 -i "$REPOROOT"/termux-packages."$1".patch
+	patch -p1 -i "$REPOROOT/termux-packages.$1.lf.patch"
+	patch -p1 -i "$REPOROOT/termux-packages.$1.crlf.patch"
 	;;
 *)
 	echo "[!] ERROR: Unsupported option $1"
@@ -18,17 +19,17 @@ arm-vfpv3-d16)
 esac
 
 # Temporarily add this until pull request is merged
-patch -p1 -i "$REPOROOT"/generate-bootstraps.sh.patch
+patch -p1 -i "$REPOROOT/generate-bootstraps.sh.patch"
 
 # Should not need to mkdir "$REPOROOT/output" if build-package.sh worked
 
-./scripts/generate-bootstraps.sh -c "$REPOROOT"/output --architectures "$TERMUX_ARCH"
+./scripts/generate-bootstraps.sh -c "$REPOROOT/output" --architectures "$TERMUX_ARCH"
 
 echo "[*] Moving bootstrap archive to $REPOROOT..."
 
 case "$1" in
 arm-vfpv3-d16)
-	mv -v bootstrap-arm.zip "$REPOROOT"/bootstrap-"$1".zip
+	mv -v bootstrap-arm.zip "$REPOROOT/bootstrap-$1.zip"
 	;;
 *)
 	mv -v bootstrap-*.zip "$REPOROOT"
