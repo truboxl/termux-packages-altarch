8f8c1ae851852a5ea52cbc597dfeb27f73d62633
Jia Yuan Lo <jylo06g@gmail.com>
2022-08-30 00:38:39 +0800

    ci: really force use 23c for arm-vfpv3-d16

--- build-package.sh
+++ build-package.sh
@@ -13,15 +13,13 @@ arm-vfpv3-d16)
 	patch -p1 -i "$REPOROOT/termux-packages.$1.lf.patch"
 	patch -p1 -i "$REPOROOT/termux-packages.$1.crlf.patch"
 	./scripts/run-docker.sh env TERMUX_NDK_VERSION_NUM=23 TERMUX_NDK_REVISION=c ./scripts/setup-android-sdk.sh
+	rm -fr "$REPOROOT/output"
+	./scripts/run-docker.sh env TERMUX_NDK_VERSION_NUM=23 TERMUX_NDK_REVISION=c ./build-package.sh -a "$TERMUX_ARCH" "$2"
 	;;
 *)
 	echo "[!] ERROR: Unsupported option $1"
 	exit 1
 esac
-
-rm -fr "$REPOROOT/output"
-
-./scripts/run-docker.sh ./build-package.sh -a "$TERMUX_ARCH" "$2"
 
 echo "[*] Moving packages..."
 mv "output" "$REPOROOT/output"
